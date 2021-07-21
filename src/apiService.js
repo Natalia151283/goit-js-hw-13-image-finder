ac55bcd9d9926ff30d645f96bf278a22b0538057
Natalia151283 <2371340@gmail.com>
2021-07-21 22:16:10 +0300

    two commit

---
+++ src/apiService.js
@@ -0,0 +1,37 @@
+const API_KEY = '22573118-ce6a930964104bc915d64b0ed';
+const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal'
+export default class newApiServer{
+    constructor(){
+        this.searchQuery = ''
+        this.page=1
+        }
+    fetchArticles(){
+        console.log('до запроса', this)
+        const url = `${BASE_URL}&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+     return fetch(url)
+        .then(r => r.json())
+        .then(({hits})=> {
+            this.incremenPage();
+           return hits;
+        });
+        }
+
+        incremenPage(){
+            this.page += 1;
+        }
+
+         resetPage(){    
+        this.page = 1;
+        }
+
+        get query (){
+            return this.searchQuery;
+        }
+        set query(newQuery){
+            this.searchQuery = newQuery;
+        }
+    }
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
+import cardGallery from './templates/cards.hbs';
+import './sass/main.scss';
+import LoadMoreBtn from './load-more-btn.js'
+import NewsApiService from './apiService.js'
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+const basicLightbox = require('basiclightbox');
+
+const  refs = {
+    searchForm: document.querySelector('#search-form'),
+    galleryConteiner: document.querySelector('.gallery'),
+    // loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+    // listItem: document.querySelector('.gallery-item')
+}
+
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
+
+const newsApiService = new NewsApiService();
+
+refs.searchForm.addEventListener('submit',onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles)
+
+
+function onSearch(e){
+    e.preventDefault();
+
+newsApiService.query = e.currentTarget.elements.query.value;
+
+if(newsApiService.query === ''){
+    return alert('Не правильно введен текст!')
+}
+loadMoreBtn.show();
+newsApiService.resetPage();
+clearHitsConteiner();
+fetchArticles()
+
+}
+
+function fetchArticles(){
+    loadMoreBtn.disable();   
+    newsApiService.fetchArticles().then(hits => {
+    appenHits(hits);
+    loadMoreBtn.enable();
+    scrolEl();
+
+});  
+}
+function appenHits(hits){
+    refs.galleryConteiner.insertAdjacentHTML('beforeend', cardGallery(hits) )
+}
+
+function clearHitsConteiner(){
+    refs.galleryConteiner.innerHTML = '';
+}
+
+
+function scrolEl(){
+loadMoreBtn.refs.button.scrollIntoView({
+  behavior: 'smooth',
+  block: 'end',
+});
+}
+
 
